4869a910743c60712c909719a8a30c8713ec4f28
Clint-Galendez-20210021497 <20210021497@my.xu.edu.ph>
2024-06-09 12:13:42 +0800

    added central content

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import Panel from 'primevue/panel'
 </script>
 
 <template>
-  <Panel>
-
-  </Panel>
+  <div class="w-screen h-[calc(100vh-140px)]">
+    <div class="rounded-lg h-full justify-items-start bg-white flex m-5">
+      <span class="self-center text-5xl ml-5">Wild Rift Counter Pick Chooser</span>
+    </div>
+  </div>
 </template>
